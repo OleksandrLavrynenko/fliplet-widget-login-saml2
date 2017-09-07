b1a90c51e2b26a484419b529e8e2199656e4d2f7
hcarneiro <hcarneiro@weboo.biz>
2017-09-07 12:47:36 +0100

    Better error handling and logged in state

--- js/build.js
+++ js/build.js
@@ -1,35 +1,54 @@
-Fliplet.Widget.instance('sso-saml', function (data) {
+Fliplet.Widget.instance('sso-saml', function(data) {
   // Load session and prepare cookie
   Fliplet.Session.get();
+  $('.sso-login').fadeIn(250);
 
-  $(this).click(function (event) {
+  $(this).click(function(event) {
     event.preventDefault();
 
+    $('.sso-error-holder').addClass('hidden');
+    $('.sso-error-warning').addClass('hidden');
+    $('.sso-error-warning').html('');
+
     if (!data.passportType) {
-      return alert('The SSO provider has not been configured');
+      $('.sso-error-holder').append('- The single sign-on provider has not been configured.<br>');
+      $('.sso-error-warning').removeClass('hidden');
+      return;
     }
 
     var ssoProviderPackageName = 'com.fliplet.sso.' + data.passportType;
     var ssoProvider = Fliplet.Widget.get(ssoProviderPackageName);
 
     if (!ssoProvider || typeof ssoProvider.authorize !== 'function') {
-      alert('The provider ' + ssoProviderPackageName + ' has not been configured on this app.');
+      $('.sso-error-holder').append('- The provider ' + ssoProviderPackageName + ' has not been configured on this app.<br>');
+      $('.sso-error-warning').removeClass('hidden');
+
       throw new Error('Provider ' + ssoProviderPackageName + ' has not registered on Fliplet.Widget.register with an "authorize()" function.');
     }
 
     if (!data.redirectAction) {
-      alert('Please set up a navigation action to be fired on successful sign in.');
+      $('.sso-error-holder').append('- Please set up a navigation action to be fired on successful sign in.<br>');
+      $('.sso-error-warning').removeClass('hidden');
+      return;
     }
 
     ssoProvider
       .authorize()
       .then(function onAuthorized() {
-        Fliplet.Navigate.to(data.redirectAction);
+        $('.sso-login').hide();
+        $('.sso-confirmation').fadeIn(250);
       })
       .catch(function onError(err) {
         // woop woop
         console.error(err);
-        alert(err);
-      })
+        $('.sso-error-holder').html(err);
+        $('.sso-error-holder').removeClass('hidden');
+      });
   });
-})
+
+  $('[data-sso-redirect]').on('click', function() {
+    event.preventDefault();
+
+    Fliplet.Navigate.to(data.redirectAction);
+  });
+});
