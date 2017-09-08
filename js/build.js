ec32511d3d1a636f14c946ec20011346b458de62
hcarneiro <hcarneiro@weboo.biz>
2017-09-08 11:08:28 +0100

    Tweaks to warnings and errors

--- js/build.js
+++ js/build.js
@@ -6,37 +6,29 @@ Fliplet.Widget.instance('sso-saml', function(data) {
   $(this).click(function(event) {
     event.preventDefault();
 
-    $('.sso-error-holder').addClass('hidden');
-    $('.sso-error-warning').addClass('hidden');
-    $('.sso-error-warning').html('');
-
-    if (!data.passportType) {
-      $('.sso-error-holder').append('- The single sign-on provider has not been configured.<br>');
-      $('.sso-error-warning').removeClass('hidden');
+    if (!data.passportType || !data.redirectAction) {
+      console.warn('Incomplete component configuration');
       return;
     }
+
+    $('.sso-error-holder').addClass('hidden');
 
     var ssoProviderPackageName = 'com.fliplet.sso.' + data.passportType;
     var ssoProvider = Fliplet.Widget.get(ssoProviderPackageName);
 
     if (!ssoProvider || typeof ssoProvider.authorize !== 'function') {
-      $('.sso-error-holder').append('- The provider ' + ssoProviderPackageName + ' has not been configured on this app.<br>');
-      $('.sso-error-warning').removeClass('hidden');
-
       throw new Error('Provider ' + ssoProviderPackageName + ' has not registered on Fliplet.Widget.register with an "authorize()" function.');
-    }
-
-    if (!data.redirectAction) {
-      $('.sso-error-holder').append('- Please set up a navigation action to be fired on successful sign in.<br>');
-      $('.sso-error-warning').removeClass('hidden');
-      return;
     }
 
     ssoProvider
       .authorize()
       .then(function onAuthorized() {
         $('.sso-login').hide();
-        $('.sso-confirmation').fadeIn(250);
+        $('.sso-confirmation').fadeIn(250, function() {
+          setTimeout(function() {
+            Fliplet.Navigate.to(data.redirectAction);
+          }, 100);
+        });
       })
       .catch(function onError(err) {
         // woop woop
@@ -44,11 +36,5 @@ Fliplet.Widget.instance('sso-saml', function(data) {
         $('.sso-error-holder').html(err);
         $('.sso-error-holder').removeClass('hidden');
       });
-  });
-
-  $('[data-sso-redirect]').on('click', function() {
-    event.preventDefault();
-
-    Fliplet.Navigate.to(data.redirectAction);
   });
 });
