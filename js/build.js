78e68ae76a59a04cdca9340d78366a42736903fd
Tony Wu <twu@fliplet.com>
2018-11-22 18:14:56 +0800

    Saves as much info as possible from response

--- js/build.js
+++ js/build.js
@@ -49,15 +49,12 @@ Fliplet.Widget.instance('sso-saml', function(data) {
             organizationId: Fliplet.Env.get('organizationId'),
             region: Fliplet.User.getAuthToken().substr(0,2)
           };
-          
-          if (response.user.id) {
-            user.id = response.user.id;
-          } else if (response.user.email) {
-            user.email = response.user.email;
-          } else {
-            user.firstName = response.user.firstName;
-            user.lastName = response.user.lastName;
-          }
+
+          _.forEach(['id', 'email', 'firstName', 'lastName'], function (key) {
+            if (response.user[key]) {
+              user[key] = response.user[key];
+            }
+          });
 
           return Fliplet.Profile.set({
             user: user,
@@ -74,7 +71,7 @@ Fliplet.Widget.instance('sso-saml', function(data) {
                 Fliplet.Navigate.to(data.redirectAction);
               }, 100);
             });
-          });            
+          });
         });
       });
     }).catch(function onError(err) {
